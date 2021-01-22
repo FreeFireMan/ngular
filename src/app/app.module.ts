67e2d0ed4ffd4a70be2b581bfca9649f88d77f4e
zhukmarkian <zhukmarkian.t@gmail.com>
2021-01-22 15:49:24 +0200

    vvvvvv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarsComponent } from './cars/cars.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    CarsComponent,
 
   ],
   imports: [
---
+++ src/data/cars.ts
@@ -0,0 +1,12 @@
+export const cars = [
+  { brand: 'BMW', year: '200black2', color: '', },
+  { brand: 'Mercedes', year: '2003', color: 'yellow', },
+  { brand: 'Porsche', year: '2019', color: 'green', },
+  { brand: 'Ferrari', year: '2000', color: 'red', },
+  { brand: 'Maserati', year: '2013', color: 'brown', },
+  { brand: 'Lamborghini', year: '2017', color: 'blue', },
+  {brand: 'Audi', year: '2020', color: 'white', },
+  { brand: 'Rolls-Royce', year: '2018', color: 'pink', },
+  { brand: 'Jaguar', year: '2015', color: 'purple', },
+  { brand: 'Bentley', year: '1999', color: 'sky', },
+];
